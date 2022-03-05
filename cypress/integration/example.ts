5310f8c49c7b16d0ef688864e15f87ff85cc46aa
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-03-05 12:57:16 +0100

    create tests for both apis

--- cypress/integration/example.ts
+++ cypress/integration/example.ts
@@ -1,25 +1,102 @@
 describe('#GET example', () => {
-  it('returns example object', () => {
+  it('returns weather data from Weatherbit', () => {
     // given
     apiIsAvailable();
 
     //when
-    const request = getExample();
+    const request = getWeatherData(false);
 
     //then
-    resultIs(request, { example: true });
+    resultIs(request, {
+      status: 'ok',
+      temperature: {
+        value: 3.3,
+        unit: 'Celsius',
+      },
+      humidity: {
+        value: 44.6334,
+        unit: null,
+      },
+      pressure: {
+        value: 1014.04,
+        unit: 'millibars',
+      },
+      windSpeed: {
+        value: 2.08848,
+        unit: 'kilometer per hour ',
+      },
+      source: 'Weatherbit',
+    });
+  });
+  it('returns weather data from Visual Crossing Weather', () => {
+    // given
+    apiIsAvailable();
+
+    //when
+    const request = getWeatherData(true);
+
+    //then
+    resultIs(request, {
+      status: 'ok',
+      temperature: {
+        value: 0.6,
+        unit: 'Celsius',
+      },
+      humidity: {
+        value: 56.3,
+        unit: null,
+      },
+      pressure: {
+        value: 1025.5,
+        unit: 'millibars',
+      },
+      windSpeed: {
+        value: 5.4,
+        unit: 'kilometer per hour ',
+      },
+      source: 'Visual Crossing Weather',
+    });
   });
 });
 
+const requestBody = {
+  lat: '52.229675',
+  lon: '21.012230',
+};
+
 function apiIsAvailable() {}
 
-function getExample(): Cypress.Chainable<Cypress.Response<any>> {
-  return cy.request('GET', `http://localhost:3001/example`);
+function getWeatherData(
+  alternateSource: boolean,
+): Cypress.Chainable<Cypress.Response<any>> {
+  return cy.request('GET', `http://localhost:3333/current_weather`, {
+    ...requestBody,
+    alternateSource,
+  });
 }
 
 async function resultIs(
   request: Cypress.Chainable<Cypress.Response<any>>,
-  result: { example: boolean },
+  result: {
+    status: string;
+    temperature: {
+      value: number;
+      unit: string;
+    };
+    humidity: {
+      value: number;
+      unit: null;
+    };
+    pressure: {
+      value: number;
+      unit: string;
+    };
+    windSpeed: {
+      value: number;
+      unit: string;
+    };
+    source: string;
+  },
 ) {
   request.should((response) => {
     expect(response.body).to.eql(result);

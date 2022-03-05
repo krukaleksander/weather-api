ab3d138956cf711fe207d484d0bf0a77cfd6cdb0
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-03-05 10:16:14 +0100

    add doenv, handle one api

--- src/weather/weather.service.ts
+++ src/weather/weather.service.ts
@@ -1,10 +1,28 @@
 import { Injectable } from '@nestjs/common';
+require('dotenv').config();
+const axios = require('axios');
+
 @Injectable({})
 export class WeatherService {
-  getWeather(dto) {
+  async getWeather(dto) {
     const { lat, lon, alternateSource } = dto;
-    return {
-      msg: `lat: ${lat}, lon: ${lon} alternateSource: ${alternateSource}`,
+
+    const options = {
+      method: 'GET',
+      url: 'https://weatherbit-v1-mashape.p.rapidapi.com/current',
+      params: { lon: lon, lat: lat },
+      headers: {
+        'x-rapidapi-host': 'weatherbit-v1-mashape.p.rapidapi.com',
+        'x-rapidapi-key': process.env.WEATHERBIT_KEY,
+      },
     };
+    try {
+      const response = await axios.request(options);
+      const data = response.data;
+      const { temp: temperature, rh: humidity, pres: pressure } = data.data[0];
+      return { status: 'ok', temperature, humidity, pressure };
+    } catch (error) {
+      return { error };
+    }
   }
 }

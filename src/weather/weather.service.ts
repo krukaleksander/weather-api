44f748ad4501ca3a10e9061e0da5e2a43c5565a1
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-03-05 10:28:11 +0100

    refactor code - wheterbit is in separate file

--- src/weather/weather.service.ts
+++ src/weather/weather.service.ts
@@ -1,28 +1,10 @@
 import { Injectable } from '@nestjs/common';
-require('dotenv').config();
-const axios = require('axios');
+import { WeatherBitService } from './services/weatherbit';
 
 @Injectable({})
 export class WeatherService {
   async getWeather(dto) {
     const { lat, lon, alternateSource } = dto;
-
-    const options = {
-      method: 'GET',
-      url: 'https://weatherbit-v1-mashape.p.rapidapi.com/current',
-      params: { lon: lon, lat: lat },
-      headers: {
-        'x-rapidapi-host': 'weatherbit-v1-mashape.p.rapidapi.com',
-        'x-rapidapi-key': process.env.WEATHERBIT_KEY,
-      },
-    };
-    try {
-      const response = await axios.request(options);
-      const data = response.data;
-      const { temp: temperature, rh: humidity, pres: pressure } = data.data[0];
-      return { status: 'ok', temperature, humidity, pressure };
-    } catch (error) {
-      return { error };
-    }
+    if (!alternateSource) return WeatherBitService(lat, lon);
   }
 }

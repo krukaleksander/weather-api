996b24a181f78e1c2696dcd0c35e7b743ed0d7dc
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-03-05 09:02:37 +0100

    remove app.controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { WeatherModule } from './weather/weather.module';
 
 @Module({
   imports: [WeatherModule],
-  controllers: [AppController],
 })
 export class AppModule {}
